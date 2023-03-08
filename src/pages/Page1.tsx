c63313d1b63f8301e65d98df497ca8fe91b23569
Netanel Halevi <noti56@gmail.com>
2023-03-08 16:18:04 +0200

    try to change

--- src/pages/Page1.tsx
+++ src/pages/Page1.tsx
@@ -17,7 +17,7 @@ const Page1 = () => {
 
   return (
     <div>
-      Page1 Network only
+      Page1 Network only this change will create a refresh ?
       {JSON.stringify(first)}
     </div>
   );
